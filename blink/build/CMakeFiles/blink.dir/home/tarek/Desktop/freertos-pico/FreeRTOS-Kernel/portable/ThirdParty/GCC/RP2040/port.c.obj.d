8b14ae37931f82f658370b3f6dc093b65212d91a
Tarek Benbrahim <tbr694@gmail.com>
2024-11-25 15:05:49 +0100

    cmake files added

---
+++ blink/build/CMakeFiles/blink.dir/home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/portable/ThirdParty/GCC/RP2040/port.c.obj.d
@@ -0,0 +1,61 @@
+CMakeFiles/blink.dir/home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/portable/ThirdParty/GCC/RP2040/port.c.obj: \
+ /home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/portable/ThirdParty/GCC/RP2040/port.c \
+ /home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/include/FreeRTOS.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /home/tarek/Desktop/freertos-pico/blink/FreeRTOSConfig.h \
+ /usr/include/newlib/assert.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/include/projdefs.h \
+ /home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/include/portable.h \
+ /home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/include/deprecated_definitions.h \
+ /home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/portable/ThirdParty/GCC/RP2040/include/portmacro.h \
+ /home/tarek/pico-sdk/src/common/pico_base/include/pico.h \
+ /home/tarek/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/tarek/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /home/tarek/Desktop/freertos-pico/blink/build/generated/pico_base/pico/version.h \
+ /home/tarek/pico-sdk/src/common/pico_base/include/pico/config.h \
+ /home/tarek/Desktop/freertos-pico/blink/build/generated/pico_base/pico/config_autogen.h \
+ /home/tarek/pico-sdk/src/boards/include/boards/pico.h \
+ /home/tarek/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/portable/ThirdParty/GCC/RP2040/include/freertos_sdk_config.h \
+ /home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/portable/ThirdParty/GCC/RP2040/include/rp2040_config.h \
+ /home/tarek/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /home/tarek/pico-sdk/src/common/pico_base/include/pico/types.h \
+ /home/tarek/pico-sdk/src/common/pico_base/include/pico/error.h \
+ /home/tarek/pico-sdk/src/rp2_common/hardware_sync/include/hardware/sync.h \
+ /home/tarek/pico-sdk/src/rp2_common/hardware_base/include/hardware/address_mapped.h \
+ /home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/include/mpu_wrappers.h \
+ /home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/include/task.h \
+ /home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/include/list.h \
+ /home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/portable/ThirdParty/GCC/RP2040/include/rp2040_config.h \
+ /home/tarek/pico-sdk/src/rp2_common/pico_multicore/include/pico/multicore.h \
+ /home/tarek/pico-sdk/src/common/pico_sync/include/pico/sync.h \
+ /home/tarek/pico-sdk/src/common/pico_sync/include/pico/sem.h \
+ /home/tarek/pico-sdk/src/common/pico_sync/include/pico/lock_core.h \
+ /home/tarek/pico-sdk/src/common/pico_time/include/pico/time.h \
+ /home/tarek/pico-sdk/src/rp2_common/hardware_timer/include/hardware/timer.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/timer.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/timer.h \
+ /home/tarek/pico-sdk/src/common/pico_sync/include/pico/mutex.h \
+ /home/tarek/pico-sdk/src/common/pico_sync/include/pico/critical_section.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/sio.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/interp.h \
+ /home/tarek/pico-sdk/src/rp2_common/hardware_clocks/include/hardware/clocks.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_structs/include/hardware/structs/clocks.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/clocks.h \
+ /home/tarek/pico-sdk/src/rp2_common/hardware_exception/include/hardware/exception.h \
+ /home/tarek/pico-sdk/src/rp2_common/hardware_irq/include/hardware/irq.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/intctrl.h \
+ /home/tarek/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/m0plus.h \
+ /home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/include/event_groups.h \
+ /home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/include/timers.h \
+ /home/tarek/Desktop/freertos-pico/FreeRTOS-Kernel/include/task.h
