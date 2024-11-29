318de76b057f853824faccf193c9a604008e4d68
Tarek Benbrahim <tbr694@gmail.com>
2024-11-30 00:48:09 +0100

    oled and led blink

---
+++ oledblink/blink.c
@@ -0,0 +1,90 @@
+#include "pico/stdlib.h"
+#include "stdio_usb.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/i2c.h"
+#include "raspberry26x32.h"
+#include "ssd1306_font.h"
+#include "stdio_usb.h" 
+
+
+#define SSD1306_HEIGHT              64
+#define SSD1306_WIDTH               128
+
+#define SSD1306_I2C_ADDR            _u(0x3C)
+
+// 400 is usual, but often these can be overclocked to improve display response.
+// Tested at 1000 on both 32 and 84 pixel height devices and it worked.
+#define SSD1306_I2C_CLK             400
+//#define SSD1306_I2C_CLK             1000
+
+
+// commands (see datasheet)
+#define SSD1306_SET_MEM_MODE        _u(0x20)
+#define SSD1306_SET_COL_ADDR        _u(0x21)
+#define SSD1306_SET_PAGE_ADDR       _u(0x22)
+#define SSD1306_SET_HORIZ_SCROLL    _u(0x26)
+#define SSD1306_SET_SCROLL          _u(0x2E)
+
+#define SSD1306_SET_DISP_START_LINE _u(0x40)
+
+#define SSD1306_SET_CONTRAST        _u(0x81)
+#define SSD1306_SET_CHARGE_PUMP     _u(0x8D)
+
+#define SSD1306_SET_SEG_REMAP       _u(0xA0)
+#define SSD1306_SET_ENTIRE_ON       _u(0xA4)
+#define SSD1306_SET_ALL_ON          _u(0xA5)
+#define SSD1306_SET_NORM_DISP       _u(0xA6)
+#define SSD1306_SET_INV_DISP        _u(0xA7)
+#define SSD1306_SET_MUX_RATIO       _u(0xA8)
+#define SSD1306_SET_DISP            _u(0xAE)
+#define SSD1306_SET_COM_OUT_DIR     _u(0xC0)
+#define SSD1306_SET_COM_OUT_DIR_FLIP _u(0xC0)
+
+#define SSD1306_SET_DISP_OFFSET     _u(0xD3)
+#define SSD1306_SET_DISP_CLK_DIV    _u(0xD5)
+#define SSD1306_SET_PRECHARGE       _u(0xD9)
+#define SSD1306_SET_COM_PIN_CFG     _u(0xDA)
+#define SSD1306_SET_VCOM_DESEL      _u(0xDB)
+
+#define SSD1306_PAGE_HEIGHT         _u(8)
+#define SSD1306_NUM_PAGES           (SSD1306_HEIGHT / SSD1306_PAGE_HEIGHT)
+#define SSD1306_BUF_LEN             (SSD1306_NUM_PAGES * SSD1306_WIDTH)
+
+#define SSD1306_WRITE_MODE         _u(0xFE)
+#define SSD1306_READ_MODE          _u(0xFF)
+
+void vBlinkTask() {
+
+   for (;;) {
+
+      gpio_put(PICO_DEFAULT_LED_PIN, 1);
+
+      vTaskDelay(250);
+
+      gpio_put(PICO_DEFAULT_LED_PIN, 0);
+
+      vTaskDelay(250);
+
+   }
+
+}
+
+void main() {
+	stdio_init_all();
+
+   gpio_init(PICO_DEFAULT_LED_PIN);
+
+   gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+   xTaskCreate(vBlinkTask, "Blink Task", 128, NULL, 1, NULL);
+
+   vTaskStartScheduler();
+
+}
